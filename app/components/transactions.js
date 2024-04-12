d797fcaea1c2bc4cab674861cf06ecc9739b2482
Aivee Madrelejos <122317279+aiveemaem@users.noreply.github.com>
2024-04-12 17:43:49 -0600

    replace delete button

--- app/components/transactions.js
+++ app/components/transactions.js
@@ -57,9 +57,23 @@ export default function Transactions({ transactions, onDelete }) {
                   <td>
                     <button
                       onClick={(e) => onDelete(txn.id, e)}
-                      className="text-red-600 hover:text-red-800"
+                      className="btn btn-sm "
+                      title="Delete"
                     >
-                      Delete
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="red"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
                     </button>
                   </td>
                 </tr>
